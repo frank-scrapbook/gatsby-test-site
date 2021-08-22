7694df04069cfbfa5f3e55b0658a7b3beeaa7ebf
Frank Peng <frank.peng@outlook.com.au>
2021-08-23 03:30:25 +1000

    Finished the Tutorial Part 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,12 @@
 
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText } from './layout.module.css'
 
 // Instead of const Layout = (props) => {...}
 // It is const Layout = ({ pageTitle, children }) => {...}
@@ -18,18 +24,26 @@ import { Link } from 'gatsby'
 //  const children = props.children
 //}
 
+// className is also another built-in prop that React automatically knows how to handle.
+
+// By CSS naming convention, use kebab case (like .nav-links).
+// But in JS naming convention, use camel case (like navLinks).
+// When using CSS Modules with Gatsby, you can have both!
+// Your kebab-case class names in your .module.css files will
+//// auto-convert to camel-case variables that you can import in your .js files.
+
 const Layout = ({ pageTitle, children }) => {
     return (
-        <div>
+        <div className={container}>
             <title>{pageTitle}</title>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}><Link to="/">Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about">About</Link></li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitle}</h1>
+                <h1 className={heading}>{pageTitle}</h1>
                 {children}
             </main>
         </div>
--- src/pages/my-component.js
+++ src/pages/my-component.js
@@ -1,12 +1,28 @@
 // Step 1: Import React. This lets you use JSX inside your .js file.
 import * as React from 'react'
+import { title } from '../components/my-component.module.css'
 
-/* Step 2: Define your component. Note that your
-component name should start with a capital letter. */
+// /* Step 2: Define your component. Note that your
+// component name should start with a capital letter. */
+// const MyComponent = () => {
+//   return (
+//     <h1>Hi, welcome to my site!</h1>
+//   )
+// }
+
+// Generated in web browser:
+// <h1 class="my-component-module--title--2-x0B">Super Sweet Title Page</h1>
+// auto-generated class name by CSS Modules, unique across your website
+// allows theoretically another component with the .title class in the .module.css file.
+// lets you write CSS scoped to your components
+// removes the worry about selector name collisions between components.
+
 const MyComponent = () => {
-  return (
-    <h1>Hi, welcome to my site!</h1>
-  )
+    return (
+        <h1 className={title}>
+            Super Sweet Title Page
+        </h1>
+    )
 }
 
 /* Step 3: Export your component so it
