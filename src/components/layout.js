02eb406938386ee378168b7103dfdc2d013c6191
Frank Peng <frank.peng@outlook.com.au>
2021-08-28 16:57:56 +1000

    Finished Gatsby Tutorial Part 4 - Querying Data with GraphQL + gatsby-source-filesystem + mdx blog setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,13 +4,14 @@
 // To improve accessibility, using <main> element to wrap page-specific elements (<h1> heading and contents from 'children')
 
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import { 
     container,
     heading,
     navLinks,
     navLinkItem,
-    navLinkText } from './layout.module.css'
+    navLinkText,
+    siteTitle, } from './layout.module.css'
 
 // Instead of const Layout = (props) => {...}
 // It is const Layout = ({ pageTitle, children }) => {...}
@@ -33,13 +34,25 @@ import {
 //// auto-convert to camel-case variables that you can import in your .js files.
 
 const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
     return (
         <div className={container}>
-            <title>{pageTitle}</title>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
             <nav>
                 <ul className={navLinks}>
-                    <li className={navLinkItem}><Link to="/">Home</Link></li>
-                    <li className={navLinkItem}><Link to="/about">About</Link></li>
+                    <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
+                    <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
                 </ul>
             </nav>
             <main>
