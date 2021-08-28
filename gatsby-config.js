4c999c869a4760640a73ffef8e311b57fe650d14
Frank Peng <frank.peng@outlook.com.au>
2021-08-28 18:03:07 +1000

    Finished Gatsby Tutorial Part 5 - Transform Data to use MDX + Blog setup continued

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,6 @@ module.exports = {
         path: `${__dirname}/blog`,
       }
     },
+    "gatsby-plugin-mdx",
   ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,32 +1,40 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
             <p>My cool posts will go in here</p>
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+              data.allMdx.nodes.map((node) => (
+                <article key={node.id}>
+                  <h2>{node.frontmatter.title}</h2>
+                  <p>Posted: {node.frontmatter.date}</p>
+                  <MDXRenderer>
+                    {node.body}
+                  </MDXRenderer>
+                </article>
+              ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+{
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      id
+      body
+      frontmatter {
+        title
+        date(formatString: "Do MMMM YYYY")
       }
     }
   }
+}
 `
 
 export default BlogPage
